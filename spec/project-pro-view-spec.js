0390e4be6bda6f21dfe98eaaec2146e801f304ce
Armin Daraei <armindaraei.1381@gmail.com>
2026-09-13 17:12:44 +0330

    fix: ensure working CommonJS build and clean entry point

--- spec/project-pro-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import ProjectProView from '../lib/project-pro-view';
-
-describe('ProjectProView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
